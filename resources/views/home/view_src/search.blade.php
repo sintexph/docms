cfd16a1ba0e621196d29d9a2cb8322c79304facf
babojamo <kevin.loquencio@gmail.com>
2020-02-17 09:10:43 +0800

    Fix search result

--- resources/views/home/view_src/search.blade.php
+++ resources/views/home/view_src/search.blade.php
@@ -15,68 +15,12 @@
             <h5>We did not find any search result for your. Sorry!</h5>
         </center>
         @else
-        <table class="table table-bordered table-hover table-striped">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Document</th>
-                    <th>Version</th>
-                    <th class="fit">Effective Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($documents as $document)
-
-                <tr>
-                    <td class="fit">
-                        <small class="document-access"><a href="#" title="{{ $document->access_type }}">{!!
-                                $document->access_icon !!}</a></small>
-                    </td>
-                    <td>
-                        <strong>
-                            @if($document->access=="3")
-                            <a href="{{ route('home.view_document',$document->active_version->id) }}">
-                                @if($document->obsolete==true)
-                                <strike>{{ $document->document_number }}</strike>
-                                @else
-                                {{ $document->document_number }}
-                                @endif
-                            </a>
-
-                            @else
-
-                            <span>
-                                @if($document->obsolete==true)
-                                <strike>{{ $document->document_number }}</strike>
-                                @else
-                                
-                                    @if(auth()->user()!=null && auth()->user()->can('view',$document))
-                                        <a href="{{ route('home.view_document',$document->active_version->id) }}">{{ $document->document_number }}</a>
-                                    @else
-                                        {{ $document->document_number }}
-                                    @endif
-                                @endif
-                            </span>
-
-                            @endif
-                        </strong>
-                        <br>
-                        <span>{{ $document->title }}</span><br>
-
-                    </td>
-                    <td>{{ $document->active_version->version }}</td>
-                    <td class="fit">{{ $document->active_version->effective_date_formatted }}</td>
-
-                </tr>
-
-                @endforeach
-            </tbody>
-        </table>
+            @component('home.components.table')
+                @slot('documents',$documents)
+            @endcomponent
         @endif
 
-        @if(count($documents)!=0)
-        {{ $documents->appends(request()->except('page'))->links() }}
-        @endif
+ 
 
     </div>
     <div class="box-footer">
